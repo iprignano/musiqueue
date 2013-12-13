d652d1c245c9e6d8c5739a3ec4e4d529280731ac
iprignano <ivanprignano@gmail.com>
2013-12-13 18:45:46 +0100

    Removed old inject

--- src/inject/inject.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.sendMessage({}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-    }
-  }, 10);
-});
